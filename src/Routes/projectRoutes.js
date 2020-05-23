2614cf52e0572d0c445345b76246a2e89c315d61
Rails Udemy Curse <diegoamatheus@gmail.com>
2020-05-23 14:10:39 -0400

    From NodeJs to Django

--- src/Routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router({mergeParams: true});
-
-router.get("/", (req, res)=>{
-    res.json({data: "Project routes"})
-});
-
-module.exports = router;
--- src/Routes/ticketRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router({mergeParams: true});
-
-router.get("/", (req, res)=>{
-    res.json({message: "Ticket routes"})
-});
-
-module.exports = router;
