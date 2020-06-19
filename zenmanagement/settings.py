5b95bbc769d89cb56f38bc847e965732113802cc
Rails Udemy Curse <diegoamatheus@gmail.com>
2020-06-19 00:02:57 -0400

    Added other apps

--- zenmanagement/settings.py
+++ zenmanagement/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'schema.apps.SchemaConfig',
+    'security.apps.SecurityConfig',
+    'data.apps.DataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
