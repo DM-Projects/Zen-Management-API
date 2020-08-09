816ff90efd04b9b4dbc7a90215d95cf15ee034b0
Rails Udemy Curse <diegoamatheus@gmail.com>
2020-08-09 16:33:26 -0400

    Generated Ticket scaffold

--- db/migrate/20200809203240_create_tickets.rb
+++ db/migrate/20200809203240_create_tickets.rb
@@ -3,7 +3,6 @@ class CreateTickets < ActiveRecord::Migration[5.2]
     create_table :tickets do |t|
       t.string :title
       t.string :description
-      t.string :label
 
       t.timestamps
     end
