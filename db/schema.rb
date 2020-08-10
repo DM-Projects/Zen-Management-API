806ec292302a35d278f4e94b84e5fa39fa81f349
Rails Udemy Curse <diegoamatheus@gmail.com>
2020-08-10 15:00:16 -0400

    added project and user resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_203240) do
+ActiveRecord::Schema.define(version: 2020_08_10_182818) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
@@ -22,8 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_203240) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
-    t.string "role"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
