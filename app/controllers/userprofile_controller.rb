dc7a07815cf2d76bd34bbaf2be2de8c5a8a1823b
Diego Matheus <diegoamatheus@gmail.com>
2020-10-26 13:24:16 -0400

    Add new methods to profile API

--- app/controllers/userprofile_controller.rb
+++ app/controllers/userprofile_controller.rb
@@ -9,4 +9,32 @@ class UserprofileController < ApplicationController
     render json: user.issues
   end
 
+  def gettickets
+
+    id = params['id']
+
+    test = Test.new(20, "Markeloff")
+
+    puts(test.id)
+    puts(test.name)
+
+    render json: test.bit
+
+  end
+
+end
+
+class Test
+
+  attr_accessor :id, :name
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+
+  def bit
+    return {id: @id, name: @name}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :tickets
   resources :users
 
-  get "/user/issues/:id", to: "userprofile#getissues"
+  get "/profile/issues/:id", to: "userprofile#getissues"
+  get "/profile/tickets/:id", to: "userprofile#gettickets"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
